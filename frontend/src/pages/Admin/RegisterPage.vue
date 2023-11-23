af5329b2e7c9ba521faf4f0afb48c31fcbc69218
Midiala <midialabarroso@gmail.com>
2023-11-23 16:24:05 -0500

    update

--- frontend/src/pages/Admin/RegisterPage.vue
+++ frontend/src/pages/Admin/RegisterPage.vue
@@ -1,8 +1,22 @@
 <template>
-  <div>
-    
-  </div>
+  <q-page padding>
+    <h6
+      class="text-primary text-h5 text-bold text-center q-pa-none q-ma-none q-animate--scale"
+    >
+      Administración de Registros
+    </h6>
+
+    <TableUsuaro />
+  </q-page>
 </template>
+
 <script setup>
-  
+import TableUsuaro from "src/components/tablecrud/TableUsuaro.vue";
+import { onBeforeMount } from "vue";
+import { useAuthStore } from "src/stores/Auth-Store";
+const { verifyToken } = useAuthStore();
+
+onBeforeMount(async () => {
+  await verifyToken();
+});
 </script>
